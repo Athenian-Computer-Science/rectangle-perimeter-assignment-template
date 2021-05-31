1929a73853e4b4c117807f37f454692441997403
mleich <mleich@athenian.org>
2021-05-31 16:56:06 -0700

    update starter code

--- my_code.py
+++ my_code.py
@@ -4,8 +4,8 @@
 # Other sources:
 # ---------------------------------------------------------
 
-base = int(input('Enter the base: '))
-height = int(input('Enter the height: '))
-perimeter = (base + height) * 2
-print(f"The perimeter of the rectangle is {perimeter}.")
+base = input(Enter the base: )
+input(Enter the height: ) = height
+perimeter = base + height * 2
+print("The perimeter of the rectangle is {perimeter}.")
 
